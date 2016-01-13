ce4a9060d2e185af051a6e39d59ef9fab0873474
Austin Simmons <autto91@gamil.com>
2016-01-13 11:19:55 -0800

    initial commit to develop

--- src/products/views.py
+++ src/products/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views.generic.detail import DetailView
 
 # Create your views here.
+from .models import Product
+
+class ProducDetailView(DetailView):
+    model = Product
+
+def product_detail_view(reqeust, id):
+    product_instance = Product.objects.get(id=id)
+    template = 'product_detail.html'
+    context = {
+        'object': product_instance
+    }
+    return render(reqeust, template, context)
